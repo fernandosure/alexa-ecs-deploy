222ff871621cbb08d8d0372f2cbcfc3ce3c18543
fernandosure <fernandosure@yahoo.com>
2017-08-23 13:19:54 -0600

    remove security for debugging purposes

--- src/api/index.js
+++ src/api/index.js
@@ -36,7 +36,7 @@ router.use(bodyParser.json());
 //     }
 // }));
 router.use(bearerToken());
-router.use(jwtCheck);
+// router.use(jwtCheck);
 
 
 
@@ -46,7 +46,7 @@ alexaApp.express({
     // verifies requests come from amazon alexa. Must be enabled for production.
     // You can disable this if you're running a dev environment and want to POST
     // things to test behavior. enabled by default.
-    checkCert: false,
+    checkCert: true,
 
     // sets up a GET route when set to true. This is handy for testing in
     // development, but not recommended for production. disabled by default
